a807517601ae0c206587d9f95f5f9c063547e1f4
antonchuvirau <anton_chuvirau@icloud.com>
2021-09-01 19:50:58 +0300

    Notification's block changes were added

--- public/js/application.js
+++ public/js/application.js
@@ -1,2 +1,14 @@
 
 'use strict';
+
+function onAccountNotificationsOpenButtonClickHandler(evt) {
+    const target = evt.target;
+    target.nextElementSibling.classList.toggle(`account-link__notifications_is-opened`);
+}
+
+document.addEventListener(`DOMContentLoaded`, () => {
+    // VARIABLES
+    const accountNotificationsOpenButton = document.querySelector(`.account-link__value`);
+    // EVENTS
+    accountNotificationsOpenButton.addEventListener(`click`, onAccountNotificationsOpenButtonClickHandler);
+});
