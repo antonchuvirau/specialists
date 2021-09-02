07ba4c46f75b3962aff69a02b73cb0d8fa8bf077
antonchuvirau <anton_chuvirau@icloud.com>
2021-09-02 16:14:16 +0300

    Catalog was added

--- app/js/main.js
+++ app/js/main.js
@@ -5,9 +5,26 @@ function onAccountNotificationsOpenButtonClickHandler(evt) {
     target.nextElementSibling.classList.toggle(`account-link__notifications_is-opened`);
 }
 
+function onDocumentClickHandler(evt) {
+    const target = evt.target;
+
+    if (target.matches(`.b-select__header-button`)) {
+        target.parentElement.nextElementSibling.classList.toggle(`b-select__wrapper_open`);
+    }
+    if (target.matches(`.b-select__list-button`)) {
+        const targetSubListElement = target.nextElementSibling;
+        target.classList.toggle(`b-select__list-button_active`);
+        if (targetSubListElement) {
+            targetSubListElement.classList.toggle(`b-select__sub-list_open`);
+        }
+    }
+}
+
 document.addEventListener(`DOMContentLoaded`, () => {
     // VARIABLES
     const accountNotificationsOpenButton = document.querySelector(`.account-link__value`);
     // EVENTS
     accountNotificationsOpenButton.addEventListener(`click`, onAccountNotificationsOpenButtonClickHandler);
 });
+
+document.addEventListener(`click`, onDocumentClickHandler);
